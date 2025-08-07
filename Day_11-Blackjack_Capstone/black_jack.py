9bf9991aaaf17dbbf7df380f2bcbdb25557103eb
Antonio Carrillo <antonio.x.carrillo@gmail.com>
2025-08-06 23:55:07 -0500

    Day Eleven - Black Jack - Complete first draft of the project

--- Day_11-Blackjack_Capstone/black_jack.py
+++ Day_11-Blackjack_Capstone/black_jack.py
@@ -6,7 +6,7 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 # Functions
 def current_scores(user_cards, user_score, computer_card):
-    print(f"Your cards: {user_cards}, current score:{user_score}")
+    print(f"Your cards: {user_cards}, current score: {user_score}")
     print(f"Computer's first card: {computer_card}")
 
 def final_scores(user_cards, user_score, computer_cards, computer_score):
@@ -30,7 +30,7 @@ def play_game():
         print(logo)
         user_hand = choices(cards, k=2)
         user_hand_total = add_hand(user_hand)
-        computer_hand =  [ choice(cards) ]
+        computer_hand = [ choice(cards) ]
         # Adjust for double Aces starting hand
         if user_hand_total > 21:
             user_hand[0] = 1
@@ -40,7 +40,7 @@ def play_game():
         # Check for natural Blackjack
         if user_hand_total == 21:
             final_scores(user_hand, user_hand_total, computer_hand, computer_hand[0])
-            print(f"Win with a Blackjack \N{smiling face with sunglasses}")
+            print(f"Win with a Blackjack \U0001f60E")
             return play_game()
 
         # Prompt for additional user inputs & check for user bust
@@ -53,7 +53,7 @@ def play_game():
                 while user_hand_total > 21:
                     if 11 not in user_hand:
                         final_scores(user_hand, user_hand_total, computer_hand, computer_hand[0])
-                        print(f"You went over. You lose \N{loudly crying face}")
+                        print(f"You went over. You lose \U0001f62d")
                         return play_game()
                     else:
                         first_eleven = user_hand.index(11)
@@ -62,22 +62,35 @@ def play_game():
             else:
                 user_turn = False
 
-        # Computer's turn
         # Computer Draws card
+        computer_hand.append(choice(cards))
+        computer_hand_total = add_hand(computer_hand)
+        # Check for computer having two Aces
+        if computer_hand_total > 21:
+            computer_hand[0] = 1
+            computer_hand_total = add_hand(computer_hand)
+        while computer_hand_total < 17:
+            computer_hand.append(choice(cards))
+            computer_hand_total = add_hand(computer_hand)
+            while computer_hand_total > 21:
+                if 11 not in computer_hand:
+                    final_scores(user_hand, user_hand_total, computer_hand, computer_hand_total)
+                    print(f"Opponent went over. You win \U0001f601")
+                    return play_game()
+                else:
+                    first_eleven = computer_hand.index(11)
+                    computer_hand[first_eleven] = 1
 
-        # TODO 8 - Check for computer total under 17
-        # TODO 9 - Check for computer bust
-        # TODO 10 - Check for ties
-        # TODO 11 - Check for player losing
-        # TODO 12 - Make all emojis unicode
-
-        # Remaining messages
-        print(f"Lose, opponent has Blackjack \N{face screaming in fear}")
-        print(f"You lose \U0001f624")
-        print(f"Draw \N{upside-down face}")
-        print(f"Opponent went over. You win \U0001f601")
-        print(f"You win \U0001f603")
-
+        # Game Ends
+        final_scores(user_hand, user_hand_total, computer_hand, computer_hand_total)
+        if computer_hand_total == 21:
+            print(f"Lose, opponent has Blackjack \U0001f631")
+        elif computer_hand_total > user_hand_total:
+            print(f"You lose \U0001f624")
+        elif computer_hand_total == user_hand_total:
+            print(f"Draw \U0001f643")
+        else:
+            print(f"You win \U0001f603")
         continue_game = False
 
     return play_game()
